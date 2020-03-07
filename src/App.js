df0be91dd4d90058006d627f15ccf56c73f529d1
anhtuannguyen <tuananh871995@gmail.com>
2020-03-07 11:24:48 +0700

    Implement play music feature

--- src/App.js
+++ src/App.js
@@ -3,20 +3,45 @@ import TimeSlider from "react-input-slider";
 import styled from 'styled-components/macro'
 
 import "./App.scss";
-import TetImg from "./images/tet.jpg";
-import PrevIcon from "./icons/PrevIcon";
-import NextIcon from "./icons/NextIcon";
-import PauseIcon from "./icons/PauseIcon";
-import PlayIcon from "./icons/PlayIcon";
+import pauseIconSrc from "./icons/pause-icon.png";
+import playIconSrc from "./icons/play-icon.png";
 import audios from "./audios";
 
-const Test = styled.div`
-  color: red;
+const RowContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+`
+
+const SongName = styled.div`
+  font-weight: 500;
+`
+
+const ControlBtnGroup = styled.div`
+  margin-top: 1px;
+`
+
+const PlayBtn = styled.img`
+  width: 25px;
+  cursor: pointer;
+`
+
+const PauseBtn = styled.img`
+  width: 25px;
+  cursor: pointer;
+`
+
+const ProgressBar = styled.div`
+  background-color: #dadada;
+  border-radius: 5px;
+  height: 23px;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
 `
 
 const App = () => {
   const audioRef = useRef();
-  const [audioIndex, setAudioIndex] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [isPlay, setPlay] = useState(false);
@@ -47,53 +72,44 @@ const App = () => {
 
   return (
     <div className="App">
-      <Test>Hello world!!!</Test>
-      <img className="Song-Thumbnail" src={TetImg} alt="tet" />
-      <h2 className="Song-Title">{audios[audioIndex].title}</h2>
-      <p className="Singer">{audios[audioIndex].artist}</p>
-      <div className="Control-Button-Group">
-        <div
-          className="Prev-Button"
-          onClick={() => setAudioIndex((audioIndex - 1) % audios.length)}
-        >
-          <PrevIcon />
-        </div>
-        <div className="Pause-Play-Button" onClick={handlePausePlayClick}>
-          {isPlay ? <PauseIcon /> : <PlayIcon />}
-        </div>
-        <div
-          className="Next-Button"
-          onClick={() => setAudioIndex((audioIndex + 1) % audios.length)}
-        >
-          <NextIcon />
-        </div>
-      </div>
-      <TimeSlider
-        axis="x"
-        xmax={duration}
-        x={currentTime}
-        onChange={handleTimeSliderChange}
-        styles={{
-          track: {
-            backgroundColor: "#e3e3e3",
-            height: "2px",
-          },
-          active: {
-            backgroundColor: "#333",
-            height: "2px",
-          },
-          thumb: {
-            marginTop: "-3px",
-            width: "8px",
-            height: "8px",
-            backgroundColor: "#333",
-            borderRadius: 0,
-          },
-        }}
-      />
+      <RowContainer>
+        <SongName>{audios[0].title}</SongName>
+        <ControlBtnGroup onClick={handlePausePlayClick}>
+          {isPlay
+            ? <PauseBtn src={pauseIconSrc} alt="pause icon" />
+            : <PlayBtn src={playIconSrc} alt="play icon"/>}
+        </ControlBtnGroup>
+        <ProgressBar>
+          <TimeSlider
+            axis="x"
+            xmax={duration}
+            x={currentTime}
+            onChange={handleTimeSliderChange}
+            styles={{
+              track: {
+                backgroundColor: "#e3e3e3",
+                height: "3px",
+                cursor: "pointer"
+              },
+              active: {
+                backgroundColor: "#333",
+                height: "3px",
+              },
+              thumb: {
+                marginTop: "-4px",
+                width: "10px",
+                height: "10px",
+                backgroundColor: "#333",
+                borderRadius: "50%",
+                cursor: "pointer",
+              },
+            }}
+          />
+        </ProgressBar>
+      </RowContainer>
       <audio
         ref={audioRef}
-        src={audios[audioIndex].src}
+        src={audios[0].src}
         onLoadedData={handleLoadedData}
         onTimeUpdate={() => setCurrentTime(audioRef.current.currentTime)}
         onEnded={() => setPlay(false)}
